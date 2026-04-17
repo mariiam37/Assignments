e7f0b2ec17cbe36234e9531dc53948c0363ea5ab
Mariam <mmariamxmohamedd@gmail.com>
2026-04-17 16:34:41 +0200

    EFC Assignment 1

--- LinQ2.cs
+++ LinQ2.cs
@@ -1,224 +1,224 @@
-﻿using Assignments.DataSource;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Assignments.DataSource;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Assignments
-{
-    internal class LinQ2
-    {
-        public static void Main()
-        {
-            var ProductList = Data.ProductList;
-            var Customers = Data.CustomerList;
+//namespace Assignments
+//{
+//    internal class LinQ2
+//    {
+//        public static void Main()
+//        {
+//            var ProductList = Data.ProductList;
+//            var Customers = Data.CustomerList;
 
-            //Q1
-            Console.WriteLine("Question 1:");
-            var top3 = ProductList
-            .OrderByDescending(p => p.UnitPrice)
-            .Take(3);
+//            //Q1
+//            Console.WriteLine("Question 1:");
+//            var top3 = ProductList
+//            .OrderByDescending(p => p.UnitPrice)
+//            .Take(3);
 
-            foreach (var p in top3)
-                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
-            Console.WriteLine();
+//            foreach (var p in top3)
+//                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
+//            Console.WriteLine();
 
-            //Q2
-            Console.WriteLine("Question 2:");
-            var page2 = ProductList
-                .Skip(5)
-                .Take(5);
+//            //Q2
+//            Console.WriteLine("Question 2:");
+//            var page2 = ProductList
+//                .Skip(5)
+//                .Take(5);
 
-            foreach (var p in page2)
-                Console.WriteLine(p.ProductName);
-            Console.WriteLine();
+//            foreach (var p in page2)
+//                Console.WriteLine(p.ProductName);
+//            Console.WriteLine();
 
-            //Q3
-            Console.WriteLine("Question 3:");
-            var less25 = ProductList
-                .OrderBy(p => p.UnitPrice)
-                .TakeWhile(p => p.UnitPrice < 25);
+//            //Q3
+//            Console.WriteLine("Question 3:");
+//            var less25 = ProductList
+//                .OrderBy(p => p.UnitPrice)
+//                .TakeWhile(p => p.UnitPrice < 25);
 
-            foreach (var p in less25)
-                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
-            Console.WriteLine();
+//            foreach (var p in less25)
+//                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
+//            Console.WriteLine();
 
-            //Q4
-            Console.WriteLine("Question 4:");
-            var allSeafoodInStock = ProductList
-                .Where(p => p.Category == "Seafood")
-                .All(p => p.UnitsInStock > 0);
-            Console.WriteLine(allSeafoodInStock);
-            Console.WriteLine();
+//            //Q4
+//            Console.WriteLine("Question 4:");
+//            var allSeafoodInStock = ProductList
+//                .Where(p => p.Category == "Seafood")
+//                .All(p => p.UnitsInStock > 0);
+//            Console.WriteLine(allSeafoodInStock);
+//            Console.WriteLine();
 
-            //Q5
-            Console.WriteLine("Question 5:");
-            int[] ids = { 3, 9, 13, 18 };
-            var contains9 = ids.Contains(9);
-            Console.WriteLine(contains9);
-            Console.WriteLine();
+//            //Q5
+//            Console.WriteLine("Question 5:");
+//            int[] ids = { 3, 9, 13, 18 };
+//            var contains9 = ids.Contains(9);
+//            Console.WriteLine(contains9);
+//            Console.WriteLine();
 
-            //Q6
-            Console.WriteLine("Question 6:");
-            var grouped = ProductList.GroupBy(p => p.Category);
-            foreach (var g in grouped)
-            {
-                Console.WriteLine(g.Key + " Count: " + g.Count());
-            }
-            Console.WriteLine();
+//            //Q6
+//            Console.WriteLine("Question 6:");
+//            var grouped = ProductList.GroupBy(p => p.Category);
+//            foreach (var g in grouped)
+//            {
+//                Console.WriteLine(g.Key + " Count: " + g.Count());
+//            }
+//            Console.WriteLine();
 
-            //Q7
-            Console.WriteLine("Question 7:");
-            var groupedNames = ProductList
-                .GroupBy(p => p.Category)
-                .Select(g => new { Category = g.Key, Names = g.Select(p => p.ProductName) });
+//            //Q7
+//            Console.WriteLine("Question 7:");
+//            var groupedNames = ProductList
+//                .GroupBy(p => p.Category)
+//                .Select(g => new { Category = g.Key, Names = g.Select(p => p.ProductName) });
 
-            foreach (var g in groupedNames)
-            {
-                Console.WriteLine(g.Category);
-                foreach (var name in g.Names)
-                    Console.WriteLine(name);
-            }
-            Console.WriteLine();
+//            foreach (var g in groupedNames)
+//            {
+//                Console.WriteLine(g.Category);
+//                foreach (var name in g.Names)
+//                    Console.WriteLine(name);
+//            }
+//            Console.WriteLine();
 
-            //Q8
-            Console.WriteLine("Question 8:");
-            var categories = ProductList
-                .GroupBy(p => p.Category)
-                .Where(g => g.Count() > 3);
+//            //Q8
+//            Console.WriteLine("Question 8:");
+//            var categories = ProductList
+//                .GroupBy(p => p.Category)
+//                .Where(g => g.Count() > 3);
 
-            foreach (var g in categories)
-                Console.WriteLine(g.Key);
-            Console.WriteLine();
+//            foreach (var g in categories)
+//                Console.WriteLine(g.Key);
+//            Console.WriteLine();
 
-            //Q9
-            Console.WriteLine("Question 9:");
-            var customerGroups =
-                from c in Customers
-                group c by c.Country into g
-                select new
-                {
-                    Country = g.Key,
-                    Count = g.Count(),
-                    TotalOrderValue = g.SelectMany(c => c.Orders).Sum(o => o.Total)
-                };
+//            //Q9
+//            Console.WriteLine("Question 9:");
+//            var customerGroups =
+//                from c in Customers
+//                group c by c.Country into g
+//                select new
+//                {
+//                    Country = g.Key,
+//                    Count = g.Count(),
+//                    TotalOrderValue = g.SelectMany(c => c.Orders).Sum(o => o.Total)
+//                };
 
-            foreach (var g in customerGroups)
-                Console.WriteLine(g.Country + " " + g.Count + " " + g.TotalOrderValue);
-            Console.WriteLine();
+//            foreach (var g in customerGroups)
+//                Console.WriteLine(g.Country + " " + g.Count + " " + g.TotalOrderValue);
+//            Console.WriteLine();
 
-            //Q10
-            Console.WriteLine("Question 10:");
-            var totalStock = ProductList.Sum(p => p.UnitsInStock);
-            Console.WriteLine(totalStock);
-            Console.WriteLine();
+//            //Q10
+//            Console.WriteLine("Question 10:");
+//            var totalStock = ProductList.Sum(p => p.UnitsInStock);
+//            Console.WriteLine(totalStock);
+//            Console.WriteLine();
 
-            //Q11
-            Console.WriteLine("Question 11:");
-            var minPrice = ProductList.Min(p => p.UnitPrice);
-            var maxPrice = ProductList.Max(p => p.UnitPrice);
+//            //Q11
+//            Console.WriteLine("Question 11:");
+//            var minPrice = ProductList.Min(p => p.UnitPrice);
+//            var maxPrice = ProductList.Max(p => p.UnitPrice);
 
-            Console.WriteLine("Min: " + minPrice);
-            Console.WriteLine("Max: " + maxPrice);
-            Console.WriteLine();
+//            Console.WriteLine("Min: " + minPrice);
+//            Console.WriteLine("Max: " + maxPrice);
+//            Console.WriteLine();
 
-            //Q12
-            Console.WriteLine("Question 12:");
-            var distinctCategories = ProductList
-                .Select(p => p.Category)
-                .Distinct();
+//            //Q12
+//            Console.WriteLine("Question 12:");
+//            var distinctCategories = ProductList
+//                .Select(p => p.Category)
+//                .Distinct();
 
-            foreach (var c in distinctCategories)
-                Console.WriteLine(c);
-            Console.WriteLine();
+//            foreach (var c in distinctCategories)
+//                Console.WriteLine(c);
+//            Console.WriteLine();
 
-            //Q13
-            Console.WriteLine("Question 13:");
-            int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
-            int[] setB = { 3, 6, 9, 12, 15, 13 };
+//            //Q13
+//            Console.WriteLine("Question 13:");
+//            int[] setA = { 1, 3, 5, 7, 9, 11, 13 };
+//            int[] setB = { 3, 6, 9, 12, 15, 13 };
 
-            var diff = setA.Except(setB);
+//            var diff = setA.Except(setB);
 
-            foreach (var x in diff)
-                Console.WriteLine(x);
-            Console.WriteLine();
+//            foreach (var x in diff)
+//                Console.WriteLine(x);
+//            Console.WriteLine();
 
-            //Q14
-            Console.WriteLine("Question 14:");
-            string[] list1 = { "Germany", "France", "UK", "Spain" };
-            string[] list2 = { "france", "SPAIN", "Italy" };
+//            //Q14
+//            Console.WriteLine("Question 14:");
+//            string[] list1 = { "Germany", "France", "UK", "Spain" };
+//            string[] list2 = { "france", "SPAIN", "Italy" };
 
-            var resultCountries = list1
-                .Except(list2, StringComparer.OrdinalIgnoreCase);
+//            var resultCountries = list1
+//                .Except(list2, StringComparer.OrdinalIgnoreCase);
 
-            foreach (var c in resultCountries)
-                Console.WriteLine(c);
-            Console.WriteLine();
+//            foreach (var c in resultCountries)
+//                Console.WriteLine(c);
+//            Console.WriteLine();
 
-            //Q15
-            Console.WriteLine("Question 15:");
-            var dict = ProductList.ToDictionary(p => p.ProductID);
+//            //Q15
+//            Console.WriteLine("Question 15:");
+//            var dict = ProductList.ToDictionary(p => p.ProductID);
 
-            if (dict.ContainsKey(18))
-            {
-                var p = dict[18];
-                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
-            }
-            Console.WriteLine();
+//            if (dict.ContainsKey(18))
+//            {
+//                var p = dict[18];
+//                Console.WriteLine(p.ProductName + " " + p.UnitPrice);
+//            }
+//            Console.WriteLine();
 
-            //Q16
-            Console.WriteLine("Question 16:");
-            var firstOver50 = ProductList.First(p => p.UnitPrice > 50);
-            Console.WriteLine(firstOver50.ProductName);
-            Console.WriteLine();
+//            //Q16
+//            Console.WriteLine("Question 16:");
+//            var firstOver50 = ProductList.First(p => p.UnitPrice > 50);
+//            Console.WriteLine(firstOver50.ProductName);
+//            Console.WriteLine();
 
-            //Q17
-            Console.WriteLine("Question 17:");
-            var firstOver500 = ProductList.FirstOrDefault(p => p.UnitPrice > 500);
+//            //Q17
+//            Console.WriteLine("Question 17:");
+//            var firstOver500 = ProductList.FirstOrDefault(p => p.UnitPrice > 500);
 
-            if (firstOver500 != null)
-                Console.WriteLine(firstOver500.ProductName);
-            else
-                Console.WriteLine("null");
-            Console.WriteLine();
+//            if (firstOver500 != null)
+//                Console.WriteLine(firstOver500.ProductName);
+//            else
+//                Console.WriteLine("null");
+//            Console.WriteLine();
 
-            //Q18
-            Console.WriteLine("Question 18:");
-            var table7 = Enumerable.Range(1, 10)
-                .Select(x => 7 * x);
+//            //Q18
+//            Console.WriteLine("Question 18:");
+//            var table7 = Enumerable.Range(1, 10)
+//                .Select(x => 7 * x);
 
-            foreach (var x in table7)
-                Console.WriteLine(x);
-            Console.WriteLine();
+//            foreach (var x in table7)
+//                Console.WriteLine(x);
+//            Console.WriteLine();
 
-            //Q19
-            Console.WriteLine("Question 19:");
-            var evens = Enumerable.Range(1, 30)
-                .Where(x => x % 2 == 0);
+//            //Q19
+//            Console.WriteLine("Question 19:");
+//            var evens = Enumerable.Range(1, 30)
+//                .Where(x => x % 2 == 0);
 
-            foreach (var x in evens)
-                Console.WriteLine(x);
-            Console.WriteLine();
+//            foreach (var x in evens)
+//                Console.WriteLine(x);
+//            Console.WriteLine();
 
-            //Q20
-            Console.WriteLine("Question 20:");
-            var concat = ProductList.Select(p => p.ProductName).Take(3)
-                .Concat(Customers.Select(c => c.CompanyName).Take(3));
+//            //Q20
+//            Console.WriteLine("Question 20:");
+//            var concat = ProductList.Select(p => p.ProductName).Take(3)
+//                .Concat(Customers.Select(c => c.CompanyName).Take(3));
 
-            foreach (var x in concat)
-                Console.WriteLine(x);
-            Console.WriteLine();
+//            foreach (var x in concat)
+//                Console.WriteLine(x);
+//            Console.WriteLine();
 
-            //Q21
-            Console.WriteLine("Question 21:");
-            var paired = ProductList
-                .Zip(Customers, (p, c) => p.ProductName + " sold to " + c.CompanyName);
+//            //Q21
+//            Console.WriteLine("Question 21:");
+//            var paired = ProductList
+//                .Zip(Customers, (p, c) => p.ProductName + " sold to " + c.CompanyName);
 
-            foreach (var x in paired)
-                Console.WriteLine(x);
-            Console.WriteLine();
+//            foreach (var x in paired)
+//                Console.WriteLine(x);
+//            Console.WriteLine();
 
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ Migrations/20260417142204_InitialCreate.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Assignments.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Biography = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoB = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumOfPages = table.Column<int>(type: "int", nullable: false),
+                    YearPublished = table.Column<int>(type: "int", nullable: false),
+                    InStock = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    BookCategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.BookCategoryId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
